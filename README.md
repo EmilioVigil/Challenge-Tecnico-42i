9edff43d05026c32911659318d093daae54cf2cd
EmilioVigil <emivigil12@gmail.com>
2023-08-01 16:30:21 -0300

    Cambios en el readme.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CHALLENGE TÉCNICO 42I
+
+## Repositorio con las soluciones de los dos ejercicios del Challenge-Técnico de 42i.
+
+## Lo trabajé puramente con JavaScript, el código de las mismas estan en la carpeta de Ejercicios en
+
+## 2 diferentes archivos, cada una corresponde a cada ejercicio.
--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 
 // ###### CHALLENGE I
-const NonConstructibleChange = require('./Ejercicios/nonConstructibleChange')
+const FindPairWithSum = require('./Ejercicios/twoNumberSum');
+
+// const numbers = [2, 7, 11, 15];
+// const targetSum = 9;
+// const res = FindPairWithSum(numbers, targetSum)  // OUTPUT: [2, 7]
 
 
 // ###### CHALLENGE II
-const FindPairWithSum = require('./Ejercicios/twoNumberSum');
+const NonConstructibleChange = require('./Ejercicios/nonConstructibleChange')
+
+// coins = [5, 7, 1, 1, 2, 3, 22]
+// const result = NonConstructibleChange(coins);  // OUTPUT: 20
